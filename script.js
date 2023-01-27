660e360261527e6bf25f9736c0f7bcac6cb922cd
saufts <l.szwaczka90@gmail.com>
2023-01-28 00:27:44 +0100

    Add startButton and eventListener

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let playerScore = 0;
 let computerScore = 0;
 let roundCount = 0;
 
+
 function getComputerChoice() {
     let computerChoice = (Math.floor(Math.random() * 100) % 3) + 1;
 
@@ -56,6 +57,7 @@ function playRound(playerSelection, computerSelection) {
     console.log(`Computer -> ${computerSelection}`);
 }
 
+/*
 function game() {
 
     while(roundCount < 5) {
@@ -97,6 +99,7 @@ function game() {
         
     printResults();
 }
+*/
 
 function printResults() {
     console.log("Final Result: ");
@@ -112,3 +115,61 @@ function printResults() {
 
     else console.log("It's a Tie!")
 }
+
+
+
+
+function game() {
+
+    
+ startButton.parentNode.removeChild startButton);
+
+    while(roundCount < 5) {
+
+        const messageWin = `You won Round ${roundCount + 1}`;
+        const messageLose = `You lost Round ${roundCount + 1}`;
+        const messageTie = `Tie in Round ${roundCount + 1}`;
+
+        let playerSelectionPrompt = prompt("What is your choice?");
+        let roundResult = playRound(playerSelectionPrompt, getComputerChoice());
+        
+
+        if(roundResult != 3) {
+
+            console.log(`Round ${roundCount+1}:`);
+
+            if (roundResult === 0) {
+                computerScore += 1;
+                console.log("Computer W");
+                alert(messageLose);
+
+                roundCount++;
+    
+            } else if (roundResult === 1) {
+                playerScore += 1;
+                console.log("Player W");
+                alert(messageWin);
+
+                roundCount++;
+    
+            } else if (roundResult === 2) {
+                console.log("Tie");
+                alert(messageTie);
+
+                roundCount++
+            }
+        }
+    }
+        
+    printResults();
+}
+
+const startButton = document.querySelector(' startButton');
+const mainContainer = document.querySelector(".mainContainer");
+
+if startButton) startButton.addEventListener('click', game);
+
+
+
+
+
